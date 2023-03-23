db9ae84fc5948bbc30f4eb3f8e58980d006b4199
mauricio-prado <110865945+mauricio-prado@users.noreply.github.com>
2023-03-23 20:57:32 -0300

    Update electrodomesticos.js

--- electrodomesticos.js
+++ electrodomesticos.js
@@ -1,26 +1,23 @@
 class Electrodomestico {
-    constructor(nombre, precioBase, color, consumoEnergetico , peso)}
-    {
+    constructor(nombre, precioBase, color, consumoEnergetico, peso) {
       this.nombre = nombre;
       this.precioBase = precioBase;
       this.color = color;
       this.consumoEnergetico = consumoEnergetico;
       this.peso = peso;
     }
-    
-    { Electrodomestico.esDeBajoConsumo(); boolean
-      return this.consumoEnergetico < 100;
-      console.log(Electrodomestico.esDeBajoConsumo()); 
+    esDeBajoConsumo() {
+    if (miElectrodomestico.esDeBajoConsumo()) {
+    console.log("El electrodoméstico es de bajo consumo.");
+ }  else {
+    console.log("El electrodoméstico no es de bajo consumo.");
+ }
+  }
+     calcularBalance() {
+      return this.precioBase / this.peso;
     }
-    calcularBalance()
-    {
-      Electrodomestico.calcularBalance(); boolean
-        return this.precioBase / this.peso;
-      console.log(Electrodomestico.calcularBalance());
-    }
-    
-    esDeAltaGama() 
-    {
+  
+    esDeAltaGama() {
       let balance = this.calcularBalance();
       if (balance > 3) {
         console.log(`${this.nombre} es un producto de alta gama.`);
@@ -30,5 +27,11 @@ class Electrodomestico {
         return false;
       }
     }
-  
-  
+  }
+
+  const miElectrodomestico = new Electrodomestico("Lavadora", 500, "Blanco", 120, 50);
+ 
+  console.log(miElectrodomestico.esDeBajoConsumo());
+  console.log(miElectrodomestico.calcularBalance());
+  console.log(miElectrodomestico.esDeAltaGama()); 
+
