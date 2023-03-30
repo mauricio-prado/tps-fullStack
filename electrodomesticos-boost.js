b44ecfee5a92b86c51e60d478f7737f808cf2c8b
mauricio-prado <110865945+mauricio-prado@users.noreply.github.com>
2023-03-30 20:10:34 -0300

    Update electrodomesticos-boost.js

--- electrodomesticos-boost.js
+++ electrodomesticos-boost.js
@@ -1,71 +1,68 @@
-var Electrodomestico = /** @class */ (function () {
-    function Electrodomestico(nombre, precioBase, color, consumoEnergetico, peso) {
+lass Electrodomestico {
+    constructor(nombre, precioBase, color, consumoEnergetico, peso) {
         this.nombre = nombre;
         this.precioBase = precioBase;
         this.color = color;
         this.consumoEnergetico = consumoEnergetico;
         this.peso = peso;
     }
-    Electrodomestico.prototype.esDeBajoConsumo = function () {
-        if (miElectrodomestico.esDeBajoConsumo()) {
-            console.log("El electrodoméstico es de bajo consumo.");
-        }
-        else {
-            console.log("El electrodoméstico no es de bajo consumo.");
-        }
-    };
-    Electrodomestico.prototype.calcularBalance = function () {
+
+    esDeBajoConsumo() {
+        return this.consumoEnergetico < 100;
+    }
+
+    calcularBalance() {
         return this.precioBase / this.peso;
-    };
-    Electrodomestico.prototype.esDeAltaGama = function () {
-        var balance = this.calcularBalance();
+    }
+
+    esDeAltaGama() {
+        const balance = this.calcularBalance();
         if (balance > 3) {
-            console.log("".concat(this.nombre, " es un producto de alta gama."));
+            console.log(`${this.nombre} es un producto de alta gama.`);
             return true;
-        }
-        else {
-            console.log("".concat(this.nombre, " no es un producto de alta gama."));
+        } else {
+            console.log(`${this.nombre} no es un producto de alta gama.`);
             return false;
         }
-    };
-    return Electrodomestico;
-}());
-var miElectrodomestico = new Electrodomestico("Lavadora", 500, "Blanco", 120, 50);
+    }
+}
+
+class MiElectrodomestico extends Electrodomestico {
+    constructor(paramEstaPrendido, paramCanal, paramVolumen, paramMarca, paramTipo, paramColor) {
+        super("TV", 1000, "Negro", 150, 60);
+        this.estaPrendido = paramEstaPrendido;
+        this.canalActual = paramCanal;
+        this.volumenActual = paramVolumen;
+        this.marca = paramMarca;
+        this.esSmartTv = paramTipo;
+        this.color = paramColor;
+    }
+
+    prenderApagar() {
+        if (this.estaPrendido == false) {
+            this.estaPrendido = true;
+        } else {
+            this.estaPrendido = false;
+        }
+    }
+
+    setEstaPrendido(paramPrendido) {
+        this.estaPrendido = paramPrendido;
+    }
+
+    getEstaPrendido() {
+        return this.estaPrendido;
+    }
+}
+
+const miElectrodomestico = new Electrodomestico("Lavadora", 500, "Blanco", 120, 50);
 console.log(miElectrodomestico.esDeBajoConsumo());
 console.log(miElectrodomestico.calcularBalance());
 console.log(miElectrodomestico.esDeAltaGama());
-var mielectrodoméstico = /** @class */ (function () {
-    function mielectrodoméstico() {
-    }
-    return mielectrodoméstico;
-}());
-constructor(paramEstaPrendido, boolean, paramVelocidad, number, paramMarca, string, paramTipo, boolean, paramColor, string);
-{
-    this.estaPrendido = paramEstaPrendido;
-    this.canalActual = paramCanal;
-    this.volumenActual = paramVolumen;
-    this.marca = paramMarca;
-    this.esSmartTv = paramTipo;
-    this.color = paramColor;
-}
-{
-    prenderApagar();
-    void {
-        Electrodomestico:Electrodomestico.estaPrendido == false
-    };
-    {
-        this.estaPrendido = true;
-    }
-    {
-        this.estaPrendido = false;
-    }
-}
-setEstaPrendido(paramPrendido, boolean);
-void {
-    this: Electrodomestico.estaPrendido = paramPrendido
-};
-getEstaPrendido();
-boolean;
-{
-    return this.estaPrendido;
-}
+
+const miTV = new MiElectrodomestico(false, 1, 10, "Samsung", true, "Negro");
+console.log(miTV.getEstaPrendido());
+miTV.prenderApagar();
+console.log(miTV.getEstaPrendido());
+miTV.setEstaPrendido(true);
+console.log(miTV.getEstaPrendido());
