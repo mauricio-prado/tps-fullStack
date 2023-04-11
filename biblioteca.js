b03a2ce74127405664a4895524a6b187079158dd
mauricio-prado <110865945+mauricio-prado@users.noreply.github.com>
2023-04-11 19:27:26 -0300

    Update biblioteca.js

--- biblioteca.js
+++ biblioteca.js
@@ -2,68 +2,84 @@
 //La misma estará compuesta por libros (deben crear la clase libro completa y utilizarla como atributo de la Biblioteca)
 //Crear instancias de la biblioteca y a traves de la misma consultar el nombre del libro y mostrarlo por consola
 //Crear una funcionalidad libre de la biblioteca y utilizarla para mostrar algun tipo de salida por consola
+import { Libros } from "./libritos";
+class Biblioteca{
+    private nombre:string;
+    private direccion:string;
+    private telefono:number;
+    private localidad:string;
 
-     var Biblioteca = function(){
-        function Biblioteca(nombre, direccion, telefono, localidad, libros) {
-        this.nombre = nombre;
-        this.direccion = direccion;
-        this.telefono = telefono;
-        this.localidad = localidad;
-        this.libros = libros;
+    private libros:Libros[];
+
+    public constructornombre(nombre:string, direccion:string, telefono:number, localidad:string,libros:Libros[]){;
+        this.nombre =
+        this.direccion=direccion;
+        this.telefono=telefono;
+        this.localidad=localidad;
+        this.libros=libros
     }
+
+    public setNombre(nombre:string){
+        this.nombre=nombre
+    }
+    public setDireccion(direccion:string){
+        this.direccion=direccion
+    }
+    public setTelefono(telefono:number){
+        this.telefono=telefono
+    }
+    public setLocalidad(localidad:string){
+        this.localidad=localidad;
+    }
+    
+    public setLibros(libros:Libros[]){
+        this.libros=libros;
+    }
+    
+    public getNombre():string{
+        return this.nombre
+    }
+
+    public getdireccion():string{
+        return this.direccion
+    }
+
+    public getTelefono():number{
+        return this.telefono
+    }
+
+    public getLocalidad():string{
+        return this.localidad
+    }
+   
+    public getLibros():Libros[]{
+        return this.libros
+    }
+    public getInfoBiblioteca():void{
+        console.log(`
+        Datos de mi biblioteca
+        Nombre: ${this.nombre}
+        Direccion:${this.direccion}
+        Telefono:${this.telefono}
+        Localidad:${this.localidad}`)
+    }
+
+    public buscadorLibros(nombre:string):void{
+        const search =libros.map(element => element)   
+         search.forEach(element => {
+            if(element.nombreLibro === nombre){
+                console.log(element)
+            }
+         });
+    }
+
 }
-Biblioteca.prototype.setNombre = function (nombre) {
-    this.nombre = nombre;
-};
-Biblioteca.prototype.setDireccion = function (direccion) {
-    this.direccion = direccion;
-};
-Biblioteca.prototype.setTelefono = function (telefono) {
-    this.telefono = telefono;
-};
-Biblioteca.prototype.setLocalidad = function (localidad) {
-    this.localidad = localidad;
-};
-Biblioteca.prototype.setLibros = function (libros) {
-    this.libros = libros;
-};
-Biblioteca.prototype.getNombre = function () {
-    return this.nombre;
-};
-Biblioteca.prototype.getdireccion = function () {
-    return this.direccion;
-};
-Biblioteca.prototype.getTelefono = function () {
-    return this.telefono;
-};
-Biblioteca.prototype.getLocalidad = function () {
-    return this.localidad;
-};
-Biblioteca.prototype.getLibros = function () {
-    return this.libros;
-};
-Biblioteca.prototype.getInfoBiblioteca = function () {
-console.log("\n Datos de la biblioteca\n Nombre: ".concat(this.nombre, "\n Direccion:").concat(this.direccion, "\n Telefono:").concat(this.telefono, "\n Localidad:").concat(this.localidad));
-};
 
-Biblioteca.prototype.buscadorLibros = function (nombre) {
-    var search = libros.map(function (element) { return element; });
-    search.forEach(function (element) {
-        if (element.nombreLibro === nombre) {
-            console.log(element);
-        }
-    });
-};
-return Biblioteca;{
-var libros = [new libros_1.Libros("moni", "harri poter", "pluma", "aventura"),
-new libros_1.Libros("juana", "crepusculo", "10ojos", "romance"),
-new libros_1.Libros("marta", "francotirador", "ojitosAzuloes", "accion")];
+const libros:Libros[]=[new Libros("ines","cars", "rueda","infantil"),
+                       new Libros("matias","iron man","rayos","accion"),
+                    ]
+const biblioteca:Biblioteca = new Biblioteca("biblioteca raumi`s","a mitad de cuadra ",247743,"las flores",libros)
 
-var biblioteca = new Biblioteca("biblioteca Rusa", "en la esquina", 222222, "mi barrio", libros);
-biblioteca.getInfoBiblioteca();
-biblioteca.buscadorLibros("crepuculo");
-}
+biblioteca.getInfoBiblioteca()
 
-"use strict";
-exports.__esModule = true;
-var libros_1 = require("./libros");
+biblioteca.buscadorLibros("iron man")
